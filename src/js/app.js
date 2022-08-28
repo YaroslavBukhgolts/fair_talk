1e006e8164031c65de697ca89ac5493b71fc3f14
Yaroslav <yaroslav.bukhgolts@yandex.ru>
2022-08-28 15:44:51 +0300

    Инициализировал проект

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as myFunctions from './modules/functions.js';
+
+myFunctions.isWebp();
+
+// Слайдер swiper
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
