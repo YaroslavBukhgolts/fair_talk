56062ecf057c49f68e15e4ef6bc8f1b7c83f02d3
Yaroslav <yaroslav.bukhgolts@yandex.ru>
2022-10-24 18:29:02 +0300

    адаптив блока учителя

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,34 @@ mobileMenu.mobileMenuClose();
 // Swiper
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 3,
   navigation: {
     nextEl: '.next',
     prevEl: '.prev',
   },
   grabCursor: true,
   speed: 800,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    560: {
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    901: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    901: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  }
 });
